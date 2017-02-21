9ed65fb2e37acd5da668dcaebe323588437c7188
Denny Bresch <db@znuny.com>
2017-02-21 11:32:57 +0100

    Header update.

--- Custom/Kernel/Output/HTML/Preferences/Password.pm
+++ Custom/Kernel/Output/HTML/Preferences/Password.pm
@@ -1,6 +1,6 @@
 # --
-# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
-# Copyright (C) 2012-2015 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
 # --
 # $origin: https://github.com/OTRS/otrs/blob/a4d17072f6bcf137aee494fde90613af7ec40c01/Kernel/Output/HTML/Preferences/Password.pm
 # --
