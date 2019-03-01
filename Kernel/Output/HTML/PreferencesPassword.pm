fff7a14898569bdff72bacd2aafc8ed74adf5011
Denny Bresch <db@znuny.com>
2019-03-01 18:53:24 +0100

    Applied CP.

--- Kernel/Output/HTML/PreferencesPassword.pm
+++ Kernel/Output/HTML/PreferencesPassword.pm
@@ -1,12 +1,12 @@
 # --
-# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
-# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
+# Copyright (C) 2012-2019 Znuny GmbH, http://znuny.com/
 # --
-# $origin: https://github.com/OTRS/otrs/blob/084ba344639f1a3d1aebd41cf81ae35c681e36c2/Kernel/Output/HTML/PreferencesPassword.pm
+# $origin: otrs - 084ba344639f1a3d1aebd41cf81ae35c681e36c2 - Kernel/Output/HTML/PreferencesPassword.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
 # --
 
 package Kernel::Output::HTML::PreferencesPassword;
