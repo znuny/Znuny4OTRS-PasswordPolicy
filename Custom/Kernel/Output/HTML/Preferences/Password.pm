5168c4d0341761417f0399e074834fcf64385436
Denny Bresch <db@znuny.com>
2019-03-05 17:14:44 +0100

    Applied CP.

--- Custom/Kernel/Output/HTML/Preferences/Password.pm
+++ Custom/Kernel/Output/HTML/Preferences/Password.pm
@@ -1,12 +1,12 @@
 # --
-# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
-# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
+# Copyright (C) 2012-2019 Znuny GmbH, http://znuny.com/
 # --
 # $origin: otrs - 75fc6d58aa7cecaa1d49a1f7bf36b4b7b7da3595 - Kernel/Output/HTML/Preferences/Password.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
 # --
 
 package Kernel::Output::HTML::Preferences::Password;
--- doc/en/feature.md
+++ doc/en/feature.md
@@ -1,4 +1,4 @@
-# Password Policy
+# Functionality
 
 This extension is useful if strong password policies are needed, e. g. in CERT or financial environments.
 
