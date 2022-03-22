03ab20aa9c7f36660533f19d893ef88333dfcdda
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2022-03-22 15:29:58 +0100

    Applied CP.

--- Custom/Kernel/Output/HTML/Preferences/Password.pm
+++ Custom/Kernel/Output/HTML/Preferences/Password.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2001-2022 OTRS AG, https://otrs.com/
+# Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2012-2022 Znuny GmbH, http://znuny.com/
 # --
 # $origin: znuny - 6b1abd14eea02f4857d30e3fb94b5ac6382a82c9 - Kernel/Output/HTML/Preferences/Password.pm
